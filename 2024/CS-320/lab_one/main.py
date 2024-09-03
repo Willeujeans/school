2485f6c4f4692fff77101f553dd6a53a07e73f7b
Willeujeans <112419867+Willeujeans@users.noreply.github.com>
2024-09-03 14:38:07 -0600

    fix bounds checking
    this will remove the situation of elements being accessed outside of bounds when comparing the left and right side of the palindrome

--- 2024/CS-320/lab_one/main.py
+++ 2024/CS-320/lab_one/main.py
@@ -12,44 +12,62 @@ def split_in_half(pattern: list) -> list[list]:
     return [left_side, right_side]
 
 
-def reverse_order(pattern: list) -> list:
+def reverse(pattern: list) -> list:
     return pattern[::-1]
 
 
 def remove_one_element(patterns: list[list]) -> list:
     left, right = patterns[0], patterns[1]
-    for i, _ in enumerate(right):
+    for i, _ in enumerate(left):
         if left[i] != right[i]:
-            if left[i+1] == right[i]:
-                left.pop(i)
-            elif right[i+1] == left[i]:
-                right.pop(i)
+            remove_if_neighbor_can_replace(i, left, right)
     return left + right
 
 
+def remove_if_neighbor_can_replace(i, left: list, right: list):
+    if i+1 < len(left) and left[i+1] == right[i]:
+        left.pop(i)  
+    if i+1 < len(right) and right[i+1] == left[i]:
+        right.pop(i)
+
+
 def is_palindrome(pattern: list[list]) -> bool:
-    for i in range(len(pattern[1])):
+    for i in range(len(pattern[0])):
         if pattern[0][i] != pattern[1][i]:
             return False
     return True
 
 
 # Fill in find_palindrome
-def find_palindrome(pattern) -> tuple:
+def find_palindrome(pattern) -> tuple | None:
+    if len(pattern) <= 2: # check for trivial solutions
+        return None
     list_pattern = list(pattern)
     list_split = split_in_half(list_pattern)
-    lists_aligned = [list_split[0], reverse_order(list_split[1])]
-    if is_palindrome(lists_aligned):
+    lists_aligned = [list_split[0], reverse(list_split[1])]
+    
+    if is_palindrome(lists_aligned): # already a palindrome, removing the center will always retain a palinedrome
         lists_aligned[1].pop(-1)
     else:
         remove_one_element(lists_aligned)
-    result = tuple(lists_aligned[0] + reverse_order(lists_aligned[1]))
-    print(result)
+        if not is_palindrome(lists_aligned):
+            return None
+    
+    result = tuple(lists_aligned[0] + reverse(lists_aligned[1]))
     return result
 
 
 def find_palindrome_test():
-    print("TEST(1) ", (1,2,3,2,1) == find_palindrome((1,2,3,3,2,1)))
-    print("TEST(2) ", (1,2,3,3,2,1) == find_palindrome((1,2,3,3,2,1,1)))
+    print("Palindrome: Number Tests")
+    print("TEST(0) ", None == find_palindrome((1,1)))
+    print("TEST(1) ", None == find_palindrome((1,)))
+    print("TEST(2) ", None == find_palindrome((1,2,3,4,5)))
+    print("TEST(3) ", (1,1) == find_palindrome((1,1,1)))
+    print("TEST(4) ", (1,1) == find_palindrome((1,0,1)))
+    print("TEST(5) ", (1,0,1) == find_palindrome((1,0,0,1)))
+    print("TEST(6) ", (1,0,0,1) == find_palindrome((1,0,1,0,1)))
+    print("TEST(7) ", (1,2,3,2,1) == find_palindrome((1,2,3,3,2,1)))
+    print("TEST(8) ", (1,2,3,3,2,1) == find_palindrome((1,2,3,3,2,1,1)))
+    print("Palindrome: String Tests")
 
 find_palindrome_test()
