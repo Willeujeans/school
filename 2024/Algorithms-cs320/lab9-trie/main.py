1b8bfeba1ce0af2df13351d84b09e11f4d5b104a
Willeujeans <112419867+Willeujeans@users.noreply.github.com>
2024-11-14 20:51:24 -0700

    Initialize lab 9 trie

---
+++ 2024/Algorithms-cs320/lab9-trie/main.py
@@ -0,0 +1,18 @@
+class Trie:
+    def __init__(self):
+        pass
+
+    def add(self, key: str) -> bool:
+        return False
+
+    def add_keys(self, keys: tuple[str, ...]) -> int:
+        return 0
+
+    def remove(self, key: str) -> bool:
+        return False
+
+    def find(self, key: str) -> bool:
+        return False
+
+    def partial(self, prefix: str) -> set[str]:
+        return set()
